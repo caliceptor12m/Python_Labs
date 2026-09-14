65e30d35f749c4c45cc480f11fe9523930beb186
caliceptor12m <skd12345a@gmail.com>
2026-09-14 08:29:09 +0530

    Completed Python Lab 6

---
+++ lab_Suryansh.py
@@ -0,0 +1,8 @@
+# First-run data
+student = ('Asha', 21, 'BMS')
+student_name, student_age, programme = student
+
+print("First Run:")
+print("Name:", student_name)
+print("Age:", student_age)
+print("Programme:", programme)
